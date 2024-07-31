a728b7badd79679ba29fd3848494d73edf2eef83
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-31 23:34:02 +0700

    feat(navlink): add dynamic styling for NavLink

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,19 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "../styles/index.css";
 
 function RootLayout() {
 	return (
 		<>
-			<Link to="/">Home</Link> | <Link to="/blog">Blog</Link> |{" "}
-			<Link to="/about">About</Link>
+			<NavLink
+				className={({ isActive, isPending }) => {
+					return isActive ? "active" : isPending ? "pending" : "";
+				}}
+				to="/"
+			>
+				Home
+			</NavLink>{" "}
+			| <NavLink to="/blog">Blog</NavLink> |{" "}
+			<NavLink to="/about">About</NavLink>
 			<p />
 			<Outlet />
 		</>
