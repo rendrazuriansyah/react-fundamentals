6dcf3e37be1f603adfd9a69e94357eba7aea3fad
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-31 15:37:46 +0700

    feat(routes): add about page, layout, and children routes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+
 import Home from "../pages";
+import About from "../pages/about";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <Home />,
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+		],
 	},
 ]);
